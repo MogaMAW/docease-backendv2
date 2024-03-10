870fca81e889c678ec244f3784b8aee9a7550e5e
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-10 10:08:04 +0300

    fix: create session after enabling 2fa

--- src/routes/twoFARoutes.ts
+++ src/routes/twoFARoutes.ts
@@ -3,12 +3,20 @@ import { authenticateMiddleware, protect } from "../controllers/userController";
 import {
   disableTwoFA,
   enableTwoFA,
+  enableTwoFAResponse,
   verifyToken,
 } from "../controllers/twoFAController";
+import { createSessionDevice } from "../controllers/sessionDeviceController";
 
 const router = express.Router();
 
-router.post("/enable", protect, enableTwoFA);
+router.post(
+  "/enable",
+  protect,
+  enableTwoFA,
+  createSessionDevice,
+  enableTwoFAResponse
+);
 router.patch("/disable/:twofaId", protect, disableTwoFA);
 router.post("/verify", protect, verifyToken, authenticateMiddleware);
 
